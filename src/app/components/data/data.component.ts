e2a6af351cd7d7d7f5472132edb861e6233a31a0
iagszs <45643388+iagszs@users.noreply.github.com>
2019-02-23 11:15:45 -0600

    Ultimo cambio

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,96 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
+  //1P valor por defecto que le quiero poner a ese campo
+  //2P reglas de validacion
+  //3P reglas de validacion asincrona
+  forma: FormGroup;
 
-  constructor() { }
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: "Ivan",
+      apellido: "Gonzalez"
+    },
+    correo: "iags93.07@gmail.com"
+  }
+  constructor() {
+    this.forma = new FormGroup({
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [Validators.required,
+        Validators.minLength(3)]),
+        'apellido': new FormControl('', [
+          Validators.required, this.noGonzalez]),
+      }),
+      'correo': new FormControl('', [Validators.required,
+      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+      'username': new FormControl('', Validators.required, this.existeUsuario),
+      'password': new FormControl('', Validators.required),
+      'password2': new FormControl(),
+    })
+    // this.forma.setValue(this.usuario);
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ])
+  }
+  guardarCambios() {
+    console.log(this.forma.value);
+    console.log(this.forma);
+    // this.forma.reset({
+    //   nombrecompleto: {
+    //     nombre: "",
+    //     apellido: "",
 
-  ngOnInit() {
+    //   },
+    //   correo: ""
+    // });
+
+  }
+
+  noGonzalez(control: FormControl): { [s: string]: boolean } {
+    if (control.value === "Gonzalez") {
+      return {
+        noGonzalez: true
+      }
+    }
+
+    return null;
+  }
+
+  noIgual(control: FormControl): any {
+
+    console.log(this);
+
+    let forma: any = this;
+    if (control.value !== forma.controls['password'].value) {
+      return {
+        noIguales: true
+      }
+    }
+
+    return null;
+  }
+
+  existeUsuario(control: FormControl): Promise<any> | Observable<any> {
+
+    let promise = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === "iagszs") {
+            resolve({ existe: true })
+          } else {
+            resolve(null)
+          }
+        }, 3000)
+      })
+    return promise;
   }
 
 }
